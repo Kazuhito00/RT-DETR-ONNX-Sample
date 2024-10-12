65d8a177e140d20762d95544cc76d8d99368cdea
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2024-10-12 11:26:16 +0900

    型ヒント修正。

--- sample_onnx.py
+++ sample_onnx.py
@@ -6,10 +6,11 @@ import argparse
 
 import cv2
 import numpy as np
-import onnxruntime
+import onnxruntime  # type:ignore
+from typing import List, Tuple
 
 
-def get_args():
+def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--device", type=int, default=0)
@@ -39,21 +40,21 @@ def get_args():
     return args
 
 
-def main():
-    # 引数解析 #################################################################
+def main() -> None:
+    # 引数解析
     args = get_args()
-    cap_device = args.device
-    cap_width = args.width
-    cap_height = args.height
+    cap_device: int = args.device
+    cap_width: int = args.width
+    cap_height: int = args.height
 
     if args.movie is not None:
         cap_device = args.movie
-    image_path = args.image
+    image_path: str = args.image
 
-    model_path = args.model
-    score_th = args.score_th
+    model_path: str = args.model
+    score_th: float = args.score_th
 
-    unuse_gpu = args.unuse_gpu
+    unuse_gpu: bool = args.unuse_gpu
 
     # カメラ準備
     if image_path is None:
@@ -62,27 +63,31 @@ def main():
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_height)
 
     # モデルロード
+    providers: List[str]
     if unuse_gpu:
         providers = ['CPUExecutionProvider']
     else:
         providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
-    onnx_session = onnxruntime.InferenceSession(
+
+    onnx_session: onnxruntime.InferenceSession = onnxruntime.InferenceSession(
         model_path,
         providers=providers,
     )
-    input_size = onnx_session.get_inputs()[0].shape
-    input_width, input_height = input_size[3], input_size[2]
+    input_size: List[int] = onnx_session.get_inputs()[0].shape
+    input_width: int = input_size[3]
+    input_height: int = input_size[2]
 
     print(onnx_session.get_providers())
     print('input size:', input_size)
 
     if image_path is not None:
-        image = cv2.imread(image_path)
+        image: np.ndarray = cv2.imread(image_path)
         image_height, image_width, _ = image.shape
-        original_size = np.array([[image_width, image_height]], dtype=np.int64)
+        original_size: np.ndarray = np.array([[image_width, image_height]],
+                                             dtype=np.int64)
 
         # 前処理：BGR->RGB、リサイズ、正規化、NCHW
-        input_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        input_image: np.ndarray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         input_image = cv2.resize(input_image, (input_width, input_height))
         input_image = input_image.astype(np.float32) / 255.0
         input_image = np.transpose(input_image, (2, 0, 1))
@@ -98,8 +103,8 @@ def main():
         )
 
         # 推論実施
-        start_time = time.time()
-        output = onnx_session.run(
+        start_time: float = time.time()
+        output: List[np.ndarray] = onnx_session.run(
             None,
             input_feed={
                 'images': input_image,
@@ -107,9 +112,9 @@ def main():
             },
         )
         labels, bboxes, scores = output
-        elapsed_time = time.time() - start_time
+        elapsed_time: float = time.time() - start_time
 
-        # 描画 ##################################################################
+        # 描画
         image = draw_debug(
             image,
             elapsed_time,
@@ -126,11 +131,12 @@ def main():
         while True:
             start_time = time.time()
 
-            # カメラキャプチャ ################################################
+            # カメラキャプチャ
+            ret: bool
             ret, frame = cap.read()
             if not ret:
                 break
-            debug_image = copy.deepcopy(frame)
+            debug_image: np.ndarray = copy.deepcopy(frame)
             image_height, image_width, _ = frame.shape
             original_size = np.array(
                 [[image_width, image_height]],
@@ -156,7 +162,7 @@ def main():
             labels, bboxes, scores = output
             elapsed_time = time.time() - start_time
 
-            # 描画 ##################################################################
+            # 描画
             image = draw_debug(
                 debug_image,
                 elapsed_time,
@@ -167,7 +173,7 @@ def main():
             )
 
             # キー処理(ESC：終了)
-            key = cv2.waitKey(1)
+            key: int = cv2.waitKey(1)
             if key == 27:  # ESC
                 break
 
@@ -179,14 +185,14 @@ def main():
 
 
 def draw_debug(
-    image,
-    elapsed_time,
-    score_th,
-    labels,
-    bboxes,
-    scores,
-):
-    debug_image = copy.deepcopy(image)
+    image: np.ndarray,
+    elapsed_time: float,
+    score_th: float,
+    labels: np.ndarray,
+    bboxes: np.ndarray,
+    scores: np.ndarray,
+) -> np.ndarray:
+    debug_image: np.ndarray = copy.deepcopy(image)
 
     for label, bbox, score in zip(labels[0], bboxes[0], scores[0]):
         if score < score_th:
